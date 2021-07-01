1996632ea7233c680d28b4bfaf3f9b146713e23d
Javier Galarza <jegj57@gmail.com>
2021-06-30 21:10:48 -0500

    Initial structure for the cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![allow(dead_code, unused_imports)]
+
+extern crate clap;
+
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, SubCommand};
 fn main() {
-    println!("Hello, world!");
+    let _matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .get_matches();
 }
