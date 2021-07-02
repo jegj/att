b75adfc07b8733f873aaec2def0fe17e9fc06d84
Javier Galarza <jegj57@gmail.com>
2021-07-01 20:26:23 -0500

    Use anyhow on the main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![allow(dead_code, unused_imports)]
-
 extern crate clap;
 
 use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, SubCommand};
-fn main() {
+use thiserror::Error;
+fn main() -> Result<(), anyhow::Error> {
     let _matches = App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
         .get_matches();
+    Ok(())
 }
