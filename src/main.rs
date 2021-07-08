e90bff579583eeabba607f288bdcaabeb64f0d69
Javier Galarza <jegj57@gmail.com>
2021-07-07 21:33:06 -0500

    Add first subcommand basics

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
 #![allow(dead_code, unused_imports)]
 extern crate clap;
 
+use std::string;
+
+use chrono::Local;
+
+mod utils;
+
 use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, SubCommand};
 use thiserror::Error;
 fn main() -> Result<(), anyhow::Error> {
-    let _matches = App::new(crate_name!())
+    let matches = App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
+        .subcommand(
+            SubCommand::with_name("start")
+                .about("Start a new task")
+                .arg(
+                    Arg::with_name("task_name")
+                        .index(1)
+                        .value_name("task_name")
+                        .required(false)
+                        .help("Start a new task"),
+                ),
+        )
         .get_matches();
+    match matches.subcommand() {
+        ("start", Some(sub_m)) => {
+            let today = utils::autogenerate_task_name(Local::today());
+            let task_name: String = sub_m.value_of("task_name").unwrap_or(&today).to_string();
+            println!("Default {}", task_name)
+        }
+        _ => panic!("chau!"),
+    }
     Ok(())
 }
---
+++ src/utils.rs
@@ -0,0 +1,18 @@
+use chrono::{Local, TimeZone};
+
+pub fn autogenerate_task_name(current_date: chrono::Date<chrono::Local>) -> String {
+    current_date.format("task_%Y%m%d").to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_autogenerate_task_name() {
+        let mut dt = Local.ymd(1970, 1, 1);
+        assert_eq!(autogenerate_task_name(dt), "task_19700101");
+        dt = Local.ymd(2021, 5, 20);
+        assert_eq!(autogenerate_task_name(dt), "task_20210520");
+    }
+}
