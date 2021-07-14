d09ea76002c880828ca436543fefdf88b702d9b1
Javier Galarza <jegj57@gmail.com>
2021-07-13 20:37:57 -0500

    Collect all the optiosn ands arguments

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use chrono::Local;
 
 mod utils;
 
+mod actions;
+
 use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, SubCommand};
 fn main() -> Result<(), anyhow::Error> {
     let matches = App::new(crate_name!())
@@ -18,7 +20,39 @@ fn main() -> Result<(), anyhow::Error> {
                         .index(1)
                         .value_name("task_name")
                         .required(false)
-                        .help("Start a new task"),
+                        .help("Task name"),
+                )
+                .arg(
+                    Arg::with_name("task_description")
+                        .short("d")
+                        .long("description")
+                        .takes_value(true)
+                        .required(false)
+                        .help("Set a custom description to the task"),
+                )
+                .arg(
+                    Arg::with_name("start_time")
+                        .short("s")
+                        .long("start-time")
+                        .takes_value(true)
+                        .required(false)
+                        .help("Set start time to the task"),
+                )
+                .arg(
+                    Arg::with_name("end_time")
+                        .short("e")
+                        .long("end-time")
+                        .takes_value(true)
+                        .required(false)
+                        .help("Set end time on the task"),
+                )
+                .arg(
+                    Arg::with_name("only_create")
+                        .short("o")
+                        .long("only-create")
+                        .takes_value(false)
+                        .required(false)
+                        .help("No start timer on this task"),
                 ),
         )
         .get_matches();
@@ -26,7 +60,15 @@ fn main() -> Result<(), anyhow::Error> {
         ("start", Some(sub_m)) => {
             let today = utils::autogenerate_task_name(Local::today());
             let task_name: String = sub_m.value_of("task_name").unwrap_or(&today).to_string();
-            println!("Default {}", task_name)
+            let task_description = sub_m.value_of("task_description").unwrap_or("").to_string();
+            let end_time = sub_m.value_of("end_time").unwrap_or("").to_string();
+            let start_time = sub_m.value_of("start_time").unwrap_or("").to_string();
+            // let task_description: String = sub_m
+            //     .value_of("task_description")
+            println!(
+                "Default: {} {} {} {}",
+                task_name, task_description, start_time, end_time
+            )
         }
         _ => panic!("chau!"),
     }
