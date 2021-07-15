f2bdc74d8fc585d018a49d16c7863c7cde4932c5
Javier Galarza <jegj57@gmail.com>
2021-07-14 21:08:20 -0500

    Update main

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ fn main() -> Result<(), anyhow::Error> {
         .author(crate_authors!())
         .about(crate_description!())
         .subcommand(
-            SubCommand::with_name("start")
-                .about("Start a new task")
+            SubCommand::with_name("create")
+                .about("Create a new task")
                 .arg(
                     Arg::with_name("task_name")
                         .index(1)
@@ -29,30 +29,6 @@ fn main() -> Result<(), anyhow::Error> {
                         .takes_value(true)
                         .required(false)
                         .help("Set a custom description to the task"),
-                )
-                .arg(
-                    Arg::with_name("start_time")
-                        .short("s")
-                        .long("start-time")
-                        .takes_value(true)
-                        .required(false)
-                        .help("Set start time to the task"),
-                )
-                .arg(
-                    Arg::with_name("end_time")
-                        .short("e")
-                        .long("end-time")
-                        .takes_value(true)
-                        .required(false)
-                        .help("Set end time on the task"),
-                )
-                .arg(
-                    Arg::with_name("only_create")
-                        .short("o")
-                        .long("only-create")
-                        .takes_value(false)
-                        .required(false)
-                        .help("No start timer on this task"),
                 ),
         )
         .get_matches();
@@ -61,14 +37,11 @@ fn main() -> Result<(), anyhow::Error> {
             let today = utils::autogenerate_task_name(Local::today());
             let task_name: String = sub_m.value_of("task_name").unwrap_or(&today).to_string();
             let task_description = sub_m.value_of("task_description").unwrap_or("").to_string();
-            let end_time = sub_m.value_of("end_time").unwrap_or("").to_string();
-            let start_time = sub_m.value_of("start_time").unwrap_or("").to_string();
+            // let end_time = sub_m.value_of("end_time").unwrap_or("").to_string();
+            // let start_time = sub_m.value_of("start_time").unwrap_or("").to_string();
             // let task_description: String = sub_m
             //     .value_of("task_description")
-            println!(
-                "Default: {} {} {} {}",
-                task_name, task_description, start_time, end_time
-            )
+            println!("Default: {} {} ", task_name, task_description)
         }
         _ => panic!("chau!"),
     }
